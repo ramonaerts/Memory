9a094942a63e01defe29aae29884c5c5fff137ec
Aerts,Ramon R.A.R.F <ramon.aerts@student.fontys.nl>
2019-12-20 13:14:40 +0100

    started game starting logic

--- memoryclient/src/main/java/gui/Memory.java
+++ memoryclient/src/main/java/gui/Memory.java
@@ -8,15 +8,14 @@ import controllers.MemoryController;
 import interfaces.*;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import messaging.ClientHandlerFactory;
 import socketcommunication.GameClient;
@@ -29,6 +28,8 @@ public class Memory extends Application implements IMemoryGui {
     private final int BORDERSIZE = 10;
 
     private IController controller;
+
+    private String clientPlayer;
 
     private VBox main;
     private Stage loginStage;
@@ -55,7 +56,6 @@ public class Memory extends Application implements IMemoryGui {
         main = new VBox();
         main.setPrefHeight(500);
         main.setPrefWidth(300);
-        //main.setAlignment(Pos.CENTER);
 
         TextField textFieldPlayerName = new TextField();
         textFieldPlayerName.setMinWidth(INPUTWIDTH);
@@ -69,10 +69,12 @@ public class Memory extends Application implements IMemoryGui {
         Button loginButton = new Button();
         loginButton.setMinWidth(INPUTWIDTH);
         loginButton.setText("Login");
-        loginButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
+        loginButton.addEventHandler(ActionEvent.ACTION,actionEvent -> {
+            try {
                 loginPlayer(textFieldPlayerName.getText(), passwordFieldPlayerPassword.getText());
+            }
+            catch (Exception e){
+                showMessage("No connection to server, try again later");
             }
         });
         main.getChildren().add(loginButton);
@@ -88,27 +90,23 @@ public class Memory extends Application implements IMemoryGui {
     }
 
     private void loginPlayer(String username, String password){
+        clientPlayer = username;
         controller.loginPlayer(username, password);
     }
 
     public void loginResult(boolean loginresult)
     {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                if(loginresult) Lobby();
-                else showMessage("Wrong user credentials");
+        Platform.runLater(() ->
+        {
+            if(loginresult) Lobby();
+            else
+            {
+                showMessage("Wrong login credentials");
+                clientPlayer = "";
             }
         });
     }
-
-    private void showMessage(final String message) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Memory");
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
-
+    
     private ListView<String> onlinePlayers;
 
     private void Lobby()
@@ -120,13 +118,36 @@ public class Memory extends Application implements IMemoryGui {
         grid.setPadding(new Insets(BORDERSIZE,BORDERSIZE,BORDERSIZE,BORDERSIZE));
 
         Group root = new Group();
-        Scene scene1 = new Scene(root, 500, 522);
+        Scene scene1 = new Scene(root, 500, 500);
         root.getChildren().add(grid);
+        Font lobbyFont = new Font(15);
+
+        Label onlinePlayersLabel = new Label("Players in lobby:");
+        onlinePlayersLabel.setFont(new Font(12));
+        onlinePlayersLabel.setLayoutX(300);
+        onlinePlayersLabel.setLayoutY(0);
+        root.getChildren().add(onlinePlayersLabel);
 
         onlinePlayers = new ListView<>();
-        onlinePlayers.setPrefWidth(100);
-        onlinePlayers.setPrefHeight(500);
-        grid.add(onlinePlayers, 27,0,20,1);
+        onlinePlayers.setPrefWidth(200);
+        onlinePlayers.setPrefHeight(475);
+        onlinePlayers.setLayoutX(300);
+        onlinePlayers.setLayoutY(25);
+        root.getChildren().add(onlinePlayers);
+
+        Button startGameButton = new Button();
+        startGameButton.setText("Start new game");
+        startGameButton.setFont(lobbyFont);
+        startGameButton.setPrefWidth(INPUTWIDTH);
+        startGameButton.setPrefHeight(50);
+        grid.add(startGameButton, 6, 2, 1, 1);
+
+        Button searchGameButton = new Button();
+        searchGameButton.setText("Search for game");
+        searchGameButton.setFont(lobbyFont);
+        searchGameButton.setPrefWidth(INPUTWIDTH);
+        searchGameButton.setPrefHeight(50);
+        grid.add(searchGameButton, 6, 7, 1, 1);
 
         loginStage.close();
         this.lobbyStage = new Stage();
@@ -137,16 +158,21 @@ public class Memory extends Application implements IMemoryGui {
 
     public void updateLobby(List<String> players)
     {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                onlinePlayers.getItems().clear();
-                for (String player : players)
-                {
-                    onlinePlayers.getItems().add(player);
-                }
+        Platform.runLater(() -> {
+            onlinePlayers.getItems().clear();
+            for (String player : players)
+            {
+                onlinePlayers.getItems().add(player);
             }
         });
+    }
+
+
+    private void showMessage(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Memory");
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
